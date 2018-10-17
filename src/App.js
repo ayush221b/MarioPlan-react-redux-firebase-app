712756f37914c2dfa93141f320ce36b39f7399b9
ayush221b <aysuh221b@gmail.com>
2018-10-17 21:13:39 +0530

    Basic setup of signin component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './Components/layout/Navbar'
 import Dashboard from './Components/dashboard/Dashboard'
 import ProjectDetails from './Components/projects/ProjectDetails'
+import SignIn from './Components/auth/SignIn'
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
           <Switch>
             <Route path='/' exact component={Dashboard} />
             <Route path='/project/:id' component={ProjectDetails} />
+            <Route path='/signin' component={SignIn} />
           </Switch>
         </div>
       </BrowserRouter>
