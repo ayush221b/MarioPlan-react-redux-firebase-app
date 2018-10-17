58ec8f7080ddb6445eb342a9f743148d6cd1ec57
ayush221b <aysuh221b@gmail.com>
2018-10-17 20:44:17 +0530

    Initial setup for Project Details

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './Components/layout/Navbar'
 import Dashboard from './Components/dashboard/Dashboard'
+import ProjectDetails from './Components/projects/ProjectDetails'
 
 class App extends Component {
   render() {
@@ -10,7 +11,8 @@ class App extends Component {
         <div className="App">
           <Navbar/>
           <Switch>
-            <Route path='/' component={Dashboard} />
+            <Route path='/' exact component={Dashboard} />
+            <Route path='/project/:id' component={ProjectDetails} />
           </Switch>
         </div>
       </BrowserRouter>
---
+++ src/Components/projects/ProjectSummary.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+    return(
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken-3">
+                <span className="card-title">
+                    Project Title
+                </span>
+                <p>Posted by Ayush Shekhar</p>
+                <p className="grey-text">
+                    3rd September, 2am
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectSummary
