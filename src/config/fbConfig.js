f56d94a97f6ab96fc3524f89e1e8f5c7591337a0
ayush221b <aysuh221b@gmail.com>
2018-10-18 08:44:01 +0530

    nitialized firebase config

---
+++ src/config/fbConfig.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAFEDiyKr0odxhihoMwpXYQuD6LZ1pMjIE",
+    authDomain: "ayush-mario-plan.firebaseapp.com",
+    databaseURL: "https://ayush-mario-plan.firebaseio.com",
+    projectId: "ayush-mario-plan",
+    storageBucket: "ayush-mario-plan.appspot.com",
+    messagingSenderId: "563632870125"
+  };
+  firebase.initializeApp(config);
+  firebase.firestore().settings({ timestampsInSnapshots:true })
+
+  export default firebase
