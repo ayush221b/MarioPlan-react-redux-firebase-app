e585d619ebee7e3ac7e4bc0b60309c9ace943aa6
ayush221b <aysuh221b@gmail.com>
2018-10-18 09:47:08 +0530

    Added additional store enhancers

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,5 +1,5 @@
 export const createProject = (project) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, {getFirebase, getFirestore}) => {
         //make async call to database
         dispatch({type:'CREATE_PROJECT', project });
     }
